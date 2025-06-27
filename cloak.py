c301d8d30ecc2fa2ff63e7ffa0dd8b10bc36e5f0
vaidehi <127808895+FreshHades@users.noreply.github.com>
2025-06-27 13:17:33 +0530

    Create cloak.py

---
+++ cloak.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+# Load background once
+bg = None
+
+def capture_background(cap):
+    global bg
+    for i in range(30):
+        ret, bg = cap.read()
+    bg = np.flip(bg, axis=1)
+
+capture_background(cv2.VideoCapture(0))
+
+def apply_cloak_effect(frame):
+    global bg
+    frame = np.flip(frame, axis=1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Pink HSV Range
+    lower_pink = np.array([135, 50, 50])
+    upper_pink = np.array([175, 255, 255])
+
+    mask = cv2.inRange(hsv, lower_pink, upper_pink)
+    mask_inv = cv2.bitwise_not(mask)
+
+    res1 = cv2.bitwise_and(bg, bg, mask=mask)
+    res2 = cv2.bitwise_and(frame, frame, mask=mask_inv)
+    final_output = cv2.addWeighted(res1, 1, res2, 1, 0)
+
+    return final_output
